caa8aa89511d9b7345a1f6b03c2ece4631871307
Arif <arifalkharus@hotmail.com>
2023-04-16 21:59:55 +0100

    first commit

--- index.js
+++ index.js
@@ -8,35 +8,15 @@ mongoose.connect(process.env.mongobduri);
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get("/", async (req, res) => {
+  res.send(
+    "Welcome to Quran-api to find a surah simply write /surah?surah={surah number}"
+  );
+});
+
 app.get("/surah", async (req, res) => {
   const data = await Quran.findOne({ name: req.query.surah });
-  res.send(data);
-});
-app.post("/create", async (req, res) => {
-  const response = await fetch(
-    "https://api.alquran.cloud/v1/quran/quran-uthmani"
-  );
-  const quran = await response.json();
-  const { surahs } = quran.data;
-
-  surahs.forEach(
-    async (x, i) =>
-      await Quran.create({
-        name: i + 1,
-        ayahs: x.ayahs,
-      })
-  );
-
-  res.send("created");
-});
-app.put("/update", async (req, res) => {
-  await Quran.updateOne({ name: req.query.name }, { name: req.query.newname });
-  res.send("updated");
-});
-
-app.delete("/delete", async (req, res) => {
-  await Quran.deleteOne({ name: req.query.name });
-  res.send(`deleted ${req.query.name}`);
+  res.json(data);
 });
 
 app.listen(PORT, () => {
